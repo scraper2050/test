0e1438765c31f5a98afb39fb8a62649445fff797
zakariaouatab <master.web0416@gmail.com>
2020-08-14 16:20:36 +0100

    update auth.ts

--- src/action/Auth.ts
+++ src/action/Auth.ts
@@ -13,11 +13,11 @@ export const getUser: () => any = () => {
     return (dispatch: any) => {
         dispatch({ type: FETCH_START });
         axios.get('/me',
-        ).then(({ data }) => {
+        ).then(({ data }: any) => {
             console.log(" ___ getUser RESPONSE ___ ", data);
             dispatch({ type: FETCH_SUCCESS });
             dispatch({ type: USER_DATA, payload: data.user });
-        }).catch(err => {
+        }).catch((err: any) => {
             console.error("xxx getUser Request ERROR xxx", err);
             dispatch({ type: FETCH_ERROR, payload: "Error during get me request with this token" });
             dispatch({ type: SIGNOUT_USER_SUCCESS });
