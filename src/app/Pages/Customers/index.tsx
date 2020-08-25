b22a68b8604180f62495fddef910944d0c7c4412
aceberry924 <meobius123@gmail.com>
2020-08-26 00:40:05 +0800

    update customers page styling after merge

--- src/app/Pages/Customers/index.tsx
+++ src/app/Pages/Customers/index.tsx
@@ -1,63 +1,57 @@
-import React, { useState } from 'react';
-import { Link, useLocation, useHistory } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, useLocation, useHistory } from "react-router-dom";
 
-import styled from 'styled-components';
-import List from '@material-ui/core/List';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import ListItem from '@material-ui/core/ListItem';
-import SwipeableViews from 'react-swipeable-views';
+import styled from "styled-components";
+import List from "@material-ui/core/List";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import ListItem from "@material-ui/core/ListItem";
+import SwipeableViews from "react-swipeable-views";
 
-import * as CONSTANTS from '../../../constants';
+import * as CONSTANTS from "../../../constants";
 
-import Header from '../../../app/Components/Header';
-import SubHeader from '../../../app/Components/SubHeader';
-import BCTabs from '../../../app/Components/BCTabs';
-import BCTable from '../../../app/Components/BCTable';
-import Sidebar from '../../../app/Components/Sidebar';
-import ToolBarSearchInput from '../../../app/Components/ToolBarSearchInput';
+import SubHeader from "../../../app/Components/SubHeader";
+import BCTabs from "../../../app/Components/BCTabs";
+import BCTable from "../../../app/Components/BCTable";
+import Sidebar from "../../../app/Components/Sidebar";
+import ToolBarSearchInput from "../../../app/Components/ToolBarSearchInput";
 
 const LINK_DATA = [
-  { label: 'Customer List', link: '/customers/customer-list' },
-  { label: 'New Customer', link: '/customers/new-customer' },
-  { label: 'Schedule/Jobs', link: '/customers/schedule' },
+  { label: "Customer List", link: "/customers/customer-list" },
+  { label: "New Customer", link: "/customers/new-customer" },
+  { label: "Schedule/Jobs", link: "/customers/schedule" },
 ];
 
 const headCells = [
   {
-    id: 'no',
-    label: 'No',
+    id: "name",
+    label: "Name",
     sortable: true,
   },
   {
-    id: 'name',
-    label: 'Name',
+    id: "contactName",
+    label: "Contact Name",
     sortable: true,
   },
   {
-    id: 'phone',
-    label: 'Phone',
+    id: "phone",
+    label: "Phone",
     sortable: true,
   },
   {
-    id: 'email',
-    label: 'Email',
+    id: "email",
+    label: "Email",
     sortable: true,
-  },
-  {
-    id: 'view',
-    label: 'View',
-    sortable: false,
   },
 ];
 
 const table_data = [
   {
     id: 0,
-    no: 1,
-    name: 'Shelly Poehler',
-    phone: '972816823',
-    email: 'Blueclerktest@yopmail.com',
+    name: "Shelly Poehler",
+    contactName: "BlueTest",
+    phone: "972816823",
+    email: "Blueclerktest@yopmail.com",
   },
 ];
 
@@ -77,71 +71,72 @@ const CustomersPage = () => {
 
   return (
     <>
-      <Header />
-      <div className="main-container">
-        <SubHeader title="Customers">
-          <ToolBarSearchInput style={{ marginLeft: 'auto', width: '321px' }} />
-          <ImportButton variant="contained">Import</ImportButton>
-          <CustomerButton variant="contained">New Customer</CustomerButton>
-        </SubHeader>
+      <SubHeader title="Customers">
+        <ToolBarSearchInput style={{ marginLeft: "auto", width: "321px" }} />
+        <CustomerButton variant="contained">New Customer</CustomerButton>
+      </SubHeader>
 
-        <MainContainer>
-          <Sidebar>
-            <StyledList aria-label="people sidebar list">
-              {LINK_DATA.map((item, idx) => (
-                <StyledListItem
-                  key={idx}
-                  button
-                  selected={pathName === item.link}
-                  onClick={() => onClickLink(item.link)}
-                >
-                  {item.label}
-                </StyledListItem>
-              ))}
-            </StyledList>
-          </Sidebar>
+      <MainContainer>
+        <Sidebar>
+          <StyledList aria-label="customers sidebar list">
+            {LINK_DATA.map((item, idx) => {
+              if (item.label === "Customer List")
+                return (
+                  <StyledListItem
+                    key={idx}
+                    button
+                    selected={
+                      pathName === item.link || pathName === "/customers"
+                    }
+                    onClick={() => onClickLink(item.link)}
+                  >
+                    {item.label}
+                  </StyledListItem>
+                );
+              else
+                return (
+                  <StyledListItem
+                    key={idx}
+                    button
+                    selected={pathName === item.link}
+                    onClick={() => onClickLink(item.link)}
+                  >
+                    {item.label}
+                  </StyledListItem>
+                );
+            })}
+          </StyledList>
+        </Sidebar>
 
-          <PageContainer>
-            <BCTabs
-              curTab={curTab}
-              onChangeTab={handleTabChange}
-              indicatorColor="primary"
-              tabsData={[
-                { value: 0, label: 'Customer List' },
-                { value: 1, label: 'Recent Activities' },
-              ]}
-            />
-            <SwipeableViews index={curTab}>
-              <DataContainer id="0" hidden={curTab !== 0}>
-                <BCTable tableData={table_data} headCells={headCells} pagination={true} />
-              </DataContainer>
-              <DataContainer id="1" hidden={curTab !== 1}>
-                <Grid container>
-                  <Grid item xs={12}></Grid>
-                </Grid>
-              </DataContainer>
-            </SwipeableViews>
-          </PageContainer>
-        </MainContainer>
-      </div>
+        <PageContainer>
+          <BCTabs
+            curTab={curTab}
+            onChangeTab={handleTabChange}
+            indicatorColor="primary"
+            tabsData={[
+              { value: 0, label: "Customer List" },
+              { value: 1, label: "Recent Activities" },
+            ]}
+          />
+          <SwipeableViews index={curTab}>
+            <DataContainer id="0" hidden={curTab !== 0}>
+              <BCTable
+                tableData={table_data}
+                headCells={headCells}
+                pagination={true}
+              />
+            </DataContainer>
+            <DataContainer id="1" hidden={curTab !== 1}>
+              <Grid container>
+                <Grid item xs={12}></Grid>
+              </Grid>
+            </DataContainer>
+          </SwipeableViews>
+        </PageContainer>
+      </MainContainer>
     </>
   );
 };
-
-const ImportButton = styled(Button)`
-  margin-left: 25px;
-  border-radius: 2px;
-  width: 192px;
-  height: 38px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-size: 16px;
-  text-transform: initial;
-  color: ${CONSTANTS.PRIMARY_DARK};
-  background-color: ${CONSTANTS.PRIMARY_WHITE};
-  box-shadow: 0px 4px 4px ${CONSTANTS.SECONDARY_DARK_GREY};
-`;
 
 const CustomerButton = styled(Button)`
   margin-left: 25px;
--- src/app/Pages/People/Components/PeopleSidebar/index.tsx
+++ src/app/Pages/People/Components/PeopleSidebar/index.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { Link, useLocation, useHistory } from 'react-router-dom';
+import React from "react";
+import { Link, useLocation, useHistory } from "react-router-dom";
 
-import styled from 'styled-components';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import Sidebar from '../../../../Components/Sidebar';
+import styled from "styled-components";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Sidebar from "../../../../Components/Sidebar";
 
 const LINK_DATA = [
-  { label: 'Groups', link: '/people/group' },
-  { label: 'Technicians', link: '/people/technician' },
-  { label: 'Managers', link: '/people/manager' },
-  { label: 'Office Admin', link: '/people/officeadmin' },
+  { label: "Groups", link: "/people/group" },
+  { label: "Technicians", link: "/people/technician" },
+  { label: "Managers", link: "/people/manager" },
+  { label: "Office Admin", link: "/people/officeadmin" },
 ];
 
 const PeopleSidebar = (): JSX.Element => {
@@ -26,12 +26,12 @@ const PeopleSidebar = (): JSX.Element => {
     <Sidebar>
       <StyledList aria-label="people sidebar list">
         {LINK_DATA.map((item, idx) => {
-          if (item.label === 'Groups')
+          if (item.label === "Groups")
             return (
               <StyledListItem
                 key={idx}
                 button
-                selected={pathName === item.link || pathName === '/people'}
+                selected={pathName === item.link || pathName === "/people"}
                 onClick={() => onClickLink(item.link)}
               >
                 {item.label}
@@ -39,7 +39,12 @@ const PeopleSidebar = (): JSX.Element => {
             );
           else
             return (
-              <StyledListItem key={idx} button selected={pathName === item.link} onClick={() => onClickLink(item.link)}>
+              <StyledListItem
+                key={idx}
+                button
+                selected={pathName === item.link}
+                onClick={() => onClickLink(item.link)}
+              >
                 {item.label}
               </StyledListItem>
             );
