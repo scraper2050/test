4b17fb5164ec1009963df40fefc5e8380e93689a
Faraz Sarwar <faraz@sudofy.com>
2020-08-30 17:41:17 +0500

    Login signup linting solved, route lazy loading added and styles added in seperate file

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,17 @@ import DashboardPage from './app/Pages/Dashboard';
 import DateFnsUtils from '@date-io/date-fns';
 import GroupPage from './app/Pages/Employees/Group';
 import Header from './app/Components/Header';
-import LoginPage from './app/Pages/Login';
 import ManagerPage from './app/Pages/Employees/Manager';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import NewCustomerPage from './app/Pages/Customers/NewCustomer';
 import OfficeAdminPage from './app/Pages/Employees/Office';
-import React from 'react';
 import RecoverPage from './app/Pages/Recover';
 import ScheduleJobsPage from './app/Pages/Customers/schedule-jobs';
-import SignUpPage from './app/Pages/SignUp';
 import TechnicianPage from './app/Pages/Employees/Technician';
+import React, { Suspense } from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+const LoginPage = React.lazy(() => import('./app/Pages/Login/Login'));
+const SignUpPage = React.lazy(() => import('./app/Pages/SignUp/Signup'));
 
 function App() {
   return (
@@ -22,80 +22,84 @@ function App() {
       <Router>
         <div className={'App'}>
           <Switch>
-            <Route
-              component={LoginPage}
-              exact
-              path={'/'}
-            />
-            <Route
-              component={SignUpPage}
-              path={'/signup'}
-            />
-            <Route
-              component={RecoverPage}
-              path={'/recover'}
-            />
+            <Suspense fallback={<div>
+              {'Loading'}
+            </div>}>
+              <Route
+                component={LoginPage}
+                exact
+                path={'/'}
+              />
+              <Route
+                component={SignUpPage}
+                path={'/signup'}
+              />
+              <Route
+                component={RecoverPage}
+                path={'/recover'}
+              />
 
-            <Route>
-              <Header />
-              <div className={'main-container'}>
-                <Switch>
-                  <AuthRoute
-                    Component={DashboardPage}
-                    exact
-                    path={'/dashboard'}
-                  />
-                  <AuthRoute
-                    Component={GroupPage}
-                    exact
-                    path={'/employees'}
-                  />
-                  <AuthRoute
-                    Component={GroupPage}
-                    exact
-                    path={'/employees/group'}
-                  />
-                  <AuthRoute
-                    Component={TechnicianPage}
-                    exact
-                    path={'/employees/technician'}
-                  />
-                  <AuthRoute
-                    Component={ManagerPage}
-                    exact
-                    path={'/employees/manager'}
-                  />
-                  <AuthRoute
-                    Component={OfficeAdminPage}
-                    exact
-                    path={'/employees/office'}
-                  />
+              <Route>
+                <Header />
+                <div className={'main-container'}>
+                  <Switch>
+                    <AuthRoute
+                      Component={DashboardPage}
+                      exact
+                      path={'/dashboard'}
+                    />
+                    <AuthRoute
+                      Component={GroupPage}
+                      exact
+                      path={'/employees'}
+                    />
+                    <AuthRoute
+                      Component={GroupPage}
+                      exact
+                      path={'/employees/group'}
+                    />
+                    <AuthRoute
+                      Component={TechnicianPage}
+                      exact
+                      path={'/employees/technician'}
+                    />
+                    <AuthRoute
+                      Component={ManagerPage}
+                      exact
+                      path={'/employees/manager'}
+                    />
+                    <AuthRoute
+                      Component={OfficeAdminPage}
+                      exact
+                      path={'/employees/office'}
+                    />
 
-                  <AuthRoute
-                    Component={CustomersPage}
-                    exact
-                    path={'/customers'}
-                  />
-                  <AuthRoute
-                    Component={CustomersPage}
-                    exact
-                    path={'/customers/customer-list'}
-                  />
-                  <AuthRoute
-                    Component={NewCustomerPage}
-                    exact
-                    path={'/customers/new-customer'}
-                  />
+                    <AuthRoute
+                      Component={CustomersPage}
+                      exact
+                      path={'/customers'}
+                    />
+                    <AuthRoute
+                      Component={CustomersPage}
+                      exact
+                      path={'/customers/customer-list'}
+                    />
+                    <AuthRoute
+                      Component={NewCustomerPage}
+                      exact
+                      path={'/customers/new-customer'}
+                    />
 
-                  {/* For dev */}
-                  <AuthRoute
-                    Component={ScheduleJobsPage}
-                    exact
-                    path={'/customers/schedule'}
-                  />
-                </Switch>
-              </div>
-            </Route>
+                    {/* For dev */}
+                    <AuthRoute
+                      Component={ScheduleJobsPage}
+                      exact
+                      path={'/customers/schedule'}
+                    />
+                  </Switch>
+                </div>
+              </Route>
+            </Suspense>
           </Switch>
         </div>
       </Router>
