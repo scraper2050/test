e150b200ef59ce521badaefe5548a40f35b50f73
vanronaldo2 <jackwullems@outlook.com>
2020-09-05 10:05:06 +0200

    invoicing mangee

--- src/app/pages/invoicing/invoice-manage.tsx
+++ src/app/pages/invoicing/invoice-manage.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {
+    withStyles, createStyles, Theme,
+} from '@material-ui/core'
+
+import useStyles from './invoicing.styles'
+import {BCTabs, BCTab} from '../../components/bc-tab2/bc-tab'
+
 
 export default function InvoicingManager() {
+    const [curTab, setCurTab] = useState<number>(0)
+    const classes = useStyles({ back: true })
+    const handleTabChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+        setCurTab(newValue)
+    }
     return (
-        <div/>
+        <div className={classes.jobs}>
+            <BCTabs
+                value={curTab}
+                onChange={handleTabChange}
+            >
+                <BCTab label='Invoices'/>
+                <BCTab label='Estimates'/>
+            </BCTabs>
+        </div>
     )
 }
