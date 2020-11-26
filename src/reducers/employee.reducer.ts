c035ea1ffcdb82cc27157a7ee97c21c638cebc4b
everestclimber89 <everestclimber89@gmail.com>
2020-11-26 17:28:05 +0800

    fixed lint issues

--- src/reducers/employee.reducer.ts
+++ src/reducers/employee.reducer.ts
@@ -2,33 +2,30 @@ import { Reducer } from 'redux';
 import { UsersState, UsersActionType, types } from './../actions/employee/employee.types';
 
 const initialEmployees: UsersState = {
-	loading: false,
+  loading: false,
   data: []
 }
 
 export const EmployeesReducer: Reducer<any> = (state = initialEmployees, action) => {
-	switch (action.type) {
-		case UsersActionType.GET:
-			return {
-				loading: true,
-				data: initialEmployees,
-			};
-		case UsersActionType.SUCCESS:
-			return {
-				loading: false,
-				data: [...action.payload],
-			}
-		case types.SET_EMPLOYEES:
-			return {
-				loading: false,
-				data: [...action.payload],
-			}
-		case UsersActionType.FAILED:
-			return {
-				...state,
-				loading: false,
-				error: action.payload,
-			}
-	}
-	return state;
+  switch (action.type) {
+    case UsersActionType.GET:
+      return {
+        loading: true,
+        data: initialEmployees,
+      };
+    case UsersActionType.SUCCESS:
+    case types.SET_EMPLOYEES:
+      return {
+        loading: false,
+        data: [...action.payload],
+      }
+    case UsersActionType.FAILED:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      }
+    default:
+  }
+  return state;
 }
