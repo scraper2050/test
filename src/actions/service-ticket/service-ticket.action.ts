59645dd618359d5dce6f5ba6074e9019f9136f9f
Jeremy.Jamal <jeremy.jamal@ithands.biz>
2020-12-08 09:20:31 +0530

    [BLUECLERK-34] Added method to call getOpenServiceTickets api and corresponding action and reducer to add and clear the entires from the store.

--- src/actions/service-ticket/service-ticket.action.ts
+++ src/actions/service-ticket/service-ticket.action.ts
@@ -17,3 +17,44 @@ export const refreshServiceTickets = (refresh: any) => {
     'type': types.SET_REFRESH_SERVICE_TICKET_STATUS
   };
 };
+
+export const setOpenServiceTicket = (openServiceTickets: any) => {
+  return {
+    'payload': openServiceTickets,
+    'type': types.SET_OPEN_SERVICE_TICKET
+  };
+};
+
+export const setOpenServiceTicketLoading = (isLoading: any) => {
+  return {
+    'payload': isLoading,
+    'type': types.SET_OPEN_SERVICE_TICKET_LOADING
+  };
+};
+
+export const setOpenServiceTicketObject = (openTicket: any) => { 
+  return {
+    'payload': openTicket,
+    'type': types.SET_OPEN_SERVICE_TICKET_OBJECT
+  };
+};
+
+export const setClearOpenServiceTicketObject = () => { 
+  return {
+    'type': types.SET_CLEAR_OPEN_SERVICE_TICKET_OBJECT
+  };
+};
+
+export const setOpenTicketFilterState = (openTicketFilterObj: any) => { 
+  return {
+    'payload':openTicketFilterObj,
+    'type': types.SET_OPEN_TICKET_FILTER_STATE
+  };
+};
+
+export const setClearOpenTicketFilterState = (openTicketFilterObj: any) => { 
+  return {
+    'payload':openTicketFilterObj,
+    'type': types.SET_OPEN_TICKET_FILTER_STATE
+  };
+};
--- src/reducers/service-ticket.reducer.ts
+++ src/reducers/service-ticket.reducer.ts
@@ -5,7 +5,16 @@ import { types } from '../actions/service-ticket/service-ticket.types';
 const initialServiceTicket = {
   'isLoading': false,
   'refresh': true,
-  'tickets': []
+  'tickets': [],
+  'openTickets': [],
+  'openTicketObj':{}, 
+  'totalOpenTickets':0,
+  'filterTicketState': {
+    'jobTypeTitle': '',
+    'dueDate':'',
+    'customerNames':'',
+    'ticketId':''
+  }
 };
 
 export default (state = initialServiceTicket, { payload, type }: ReducerParamsInterface) => {
@@ -25,6 +34,37 @@ export default (state = initialServiceTicket, { payload, type }: ReducerParamsIn
         ...state,
         'refresh': payload
       };
+    case types.SET_OPEN_SERVICE_TICKET_LOADING:
+      return {
+        ...state,
+        'isLoading': payload
+      };
+    case types.SET_OPEN_SERVICE_TICKET:
+      return {
+        ...state,
+        'openTickets': [...payload.serviceTickets],
+        'totalOpenTickets': payload.total
+      };
+    case types.SET_OPEN_SERVICE_TICKET_OBJECT:
+      return {
+        ...state,
+        'openTicketObj': payload
+      };
+    case types.SET_CLEAR_OPEN_SERVICE_TICKET_OBJECT:
+      return {
+        ...state,
+        'openTicketObj': {}
+        };
+    case types.SET_OPEN_TICKET_FILTER_STATE:
+      return {
+        ...state,
+        'filterTicketState': payload
+        };
+    case types.SET_CLEAR_TICKET_FILTER_STATE:
+      return {
+        ...state,
+        'filterTicketState': payload
+      }
     default:
       return state;
   }
