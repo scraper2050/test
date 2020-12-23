911ef3316df41283c81357cd5623f1aec6b32c06
gustavo-reimers <gustavoreimers09@gmail.com>
2020-12-23 15:22:29 +0100

    Fix duplicate job type

--- src/app/modals/bc-add-job-type-modal/bc-add-job-type-modal.tsx
+++ src/app/modals/bc-add-job-type-modal/bc-add-job-type-modal.tsx
@@ -2,29 +2,48 @@
 import BCInput from 'app/components/bc-input/bc-input';
 import styles from './bc-add-job-type-modal.styles';
 import { useFormik } from 'formik';
-import { DialogActions, DialogContent, Fab, Grid, withStyles } from '@material-ui/core';
-import React from 'react';
+import { DialogActions, DialogContent, Fab, Grid, Typography } from '@material-ui/core';
+import { Theme, makeStyles, withStyles } from '@material-ui/core/styles';
+
+import React, {useEffect, useState} from 'react';
 import { closeModalAction, setModalDataAction } from 'actions/bc-modal/bc-modal.action';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 import { getAllJobTypesAPI, saveJobType } from 'api/job.api';
+
+const useStyles = makeStyles(theme => ({
+   'errorMsg': {
+       'padding-left': '24px'
+   }
+ }));
 
 function BCAddJobTypeModal({
    classes
 }: any): JSX.Element {
    const dispatch = useDispatch();
-
+   const jobTypesList = useSelector((state: any) => state.jobTypes);
+   const [existJobType, setExistJobType] = useState(false)
    const onSubmit = (values: any, { setSubmitting }: any) => {
       setSubmitting(true);
-      const jobType = new Promise(async (resolve, reject) => {
-         const title = values.title;
-         const savingJobType = await saveJobType({ title });
-         savingJobType.status === 1 ? resolve(savingJobType) : reject(savingJobType);
-      });
+      let result = []
+      if (jobTypesList.data.length !== 0) {
+         result = jobTypesList.data.filter((jobtype : any) => jobtype.title.toLowerCase() === values.title.toLowerCase())
+      }
+      if (result.length === 0) {
+         setExistJobType(false)
+         const jobType = new Promise(async (resolve, reject) => {
+            const title = values.title;
+            const savingJobType = await saveJobType({ title });
+            savingJobType.status === 1 ? resolve(savingJobType) : reject(savingJobType);
+         });
 
-      jobType
-         .then(res => onSuccess())
-         .catch(err => console.log(err))
-         .finally(() => setSubmitting(false));
+         jobType
+            .then(res => onSuccess())
+            .catch(err => console.log(err))
+            .finally(() => setSubmitting(false));
+      } else {
+         setExistJobType(true)
+         setSubmitting(false)
+      }
    }
 
    const form = useFormik({
@@ -56,7 +75,7 @@ function BCAddJobTypeModal({
       dispatch(closeModalAction());
       dispatch(getAllJobTypesAPI());
    }
-
+   const classStyle = useStyles();
    return (
       <form onSubmit={FormikSubmit}>
          <DialogContent classes={{ 'root': classes.dialogContent }}>
@@ -77,6 +96,13 @@ function BCAddJobTypeModal({
                </Grid>
             </Grid>
          </DialogContent>
+         {existJobType && <Typography
+            gutterBottom
+            variant={'subtitle1'}
+            color={'error'}
+            className={classStyle.errorMsg}>
+            {`This Job Type already exists`}
+         </Typography>}
          <DialogActions classes={{
             'root': classes.dialogActions
          }}>
