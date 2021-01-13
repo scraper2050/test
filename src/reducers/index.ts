78a089c5531c13fa3c28a2a9a6942d2513517177
Stephen Okpalaononuju <stephenokpala@gmail.com>
2021-01-13 08:43:05 +0100

    ft-create-job-report-BLUECLERK-14

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,47 +1,48 @@
-import { CustomersState } from './customer.types';
-import { JobsSate } from './invoicing.types';
-import { SnackbarState } from './snackbar.type';
-import auth from './auth.reducer';
-import { combineReducers } from 'redux';
-import companyEquipment from './company-equipment.reducer';
-import { CustomersReducer as customers } from './customer.reducer';
-import { ImageReducer as image} from './image.reducer';
-import { EmployeesReducer as employees }  from './employee.reducer';
-import jobState from './jobs.reducer';
-import jobTypes from './job-type.reducer';
-import modal from './bc-modal.reducer';
-import routeReducer from './route.reducer';
-import serviceTicket from './service-ticket.reducer';
-import { SnackbarReducer as snackbar } from './snackbar.reducer';
-import tax from './tax.reduxer';
-import { JobSiteReducer as jobSites } from './job-site.reducer';
-import { JobLocationReducer as jobLocations } from './job-location.reducer';
-import { EmployeesForJobReducer as employeesForJob } from './employees-for-job.reducer';
-import { VendorsReducer as vendors } from './vendor.reducer';
-import { GroupReducer as groups } from './group.reducer';
-import { TechniciansReducer as technicians } from './technicians.reducer';
-import { ManagersReducer as managers } from './managers.reducer';
-import { OfficeAdminReducer as officeAdmin } from './office-admin.reducer';
-import { InventoryReducer as inventory } from './inventory.reducer';
-import { PurchasedTagsReducer as purchasedTags } from './tags.reducer';
+import { CustomersState } from "./customer.types";
+import { JobsSate } from "./invoicing.types";
+import { SnackbarState } from "./snackbar.type";
+import auth from "./auth.reducer";
+import { combineReducers } from "redux";
+import companyEquipment from "./company-equipment.reducer";
+import { CustomersReducer as customers } from "./customer.reducer";
+import { ImageReducer as image } from "./image.reducer";
+import { EmployeesReducer as employees } from "./employee.reducer";
+import jobState from "./jobs.reducer";
+import jobTypes from "./job-type.reducer";
+import modal from "./bc-modal.reducer";
+import routeReducer from "./route.reducer";
+import serviceTicket from "./service-ticket.reducer";
+import { SnackbarReducer as snackbar } from "./snackbar.reducer";
+import tax from "./tax.reduxer";
+import { JobSiteReducer as jobSites } from "./job-site.reducer";
+import { JobLocationReducer as jobLocations } from "./job-location.reducer";
+import { EmployeesForJobReducer as employeesForJob } from "./employees-for-job.reducer";
+import { VendorsReducer as vendors } from "./vendor.reducer";
+import { GroupReducer as groups } from "./group.reducer";
+import { TechniciansReducer as technicians } from "./technicians.reducer";
+import { ManagersReducer as managers } from "./managers.reducer";
+import { OfficeAdminReducer as officeAdmin } from "./office-admin.reducer";
+import { InventoryReducer as inventory } from "./inventory.reducer";
+import { PurchasedTagsReducer as purchasedTags } from "./tags.reducer";
 import {
   InvoicingTodoReducer as invoiceTodos,
   InvoicingListReducer as invoiceList,
   InvoicingPurchaseOrderReducer as purchaseOrder,
-  InvoicingEstimatesReducer as estimates
-} from './invoicing.reducer';
-import { BrandsReducer as brands } from './brands.reducer';
-import { EquipmentTypeReducer as equipmentType } from './equipment-type.reducer';
-import { CompanyProfileReducer as profile } from './user.reducer';
+  InvoicingEstimatesReducer as estimates,
+} from "./invoicing.reducer";
+import { BrandsReducer as brands } from "./brands.reducer";
+import { EquipmentTypeReducer as equipmentType } from "./equipment-type.reducer";
+import { CompanyProfileReducer as profile } from "./user.reducer";
+import { JobReportReducer as JobReport } from "./job-report.reducer";
 
 export interface ReducerParamsInterface {
-  payload: any,
-  type: string
+  payload: any;
+  type: string;
 }
 export interface RootState {
-  jobState: JobsSate,
-  snackbarState?: SnackbarState,
-  customersState: CustomersState,
+  jobState: JobsSate;
+  snackbarState?: SnackbarState;
+  customersState: CustomersState;
 }
 export default combineReducers({
   auth,
@@ -53,7 +54,7 @@ export default combineReducers({
   jobState,
   jobTypes,
   modal,
-  'routeData': routeReducer,
+  routeData: routeReducer,
   serviceTicket,
   snackbar,
   tax,
@@ -72,5 +73,6 @@ export default combineReducers({
   equipmentType,
   jobSites,
   jobLocations,
-  employeesForJob
+  employeesForJob,
+  JobReport,
 });
