5f30a43a1f974f53e4b6dc4c97ad2eb9e563e63c
Arslan Sajid <arslansajid951@gmail.com>
2021-01-22 22:42:09 +0500

    fixed table state loading issue

--- src/app/components/bc-table-container/bc-table-content.tsx
+++ src/app/components/bc-table-container/bc-table-content.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import BCTablePagination from "./bc-table-pagination";
 import MaUTable from "@material-ui/core/Table";
 import Paper from "@material-ui/core/Paper";
@@ -34,6 +34,7 @@ function BCTableContent({
 }: any) {
   const dispatch = useDispatch();
   const currentPage = useSelector((state: any) => state.tableState.pageNumber);
+  const [showTable, setShowTable] = useState(false);
 
   const {
     getTableProps,
@@ -71,10 +72,114 @@ function BCTableContent({
   useEffect(() => {
     if (!!isPageSaveEnabled) {
       setTimeout(() => {
+        setShowTable(true);
         gotoPage(currentPage);
-      }, 500);
+      }, 100);
     }
   }, [data]);
+
+  const TableContent = () => (
+    <MaUTable size={"small"} {...getTableProps()}>
+      <TableHead>
+        {headerGroups.map((headerGroup: any, gindex: number) => (
+          <TableRow
+            key={`table-${gindex}`}
+            {...headerGroup.getHeaderGroupProps()}
+          >
+            {headerGroup.headers.map((column: any, hindex: number) => (
+              <TableCell
+                className={`whitespace-no-wrap p-12 ${
+                  column.borderRight ? "cell-border-right cursor-default" : ""
+                }`}
+                key={`table-cell-${hindex}`}
+                {...(!column.sortable
+                  ? column.getHeaderProps()
+                  : column.getHeaderProps(column.getSortByToggleProps()))}
+              >
+                {column.render("Header")}
+                {column.sortable ? (
+                  <TableSortLabel
+                    active={column.isSorted}
+                    // React-table has a unsorted state which is not treated here
+                    direction={column.isSortedDesc ? "desc" : "asc"}
+                  />
+                ) : null}
+              </TableCell>
+            ))}
+          </TableRow>
+        ))}
+      </TableHead>
+      <TableBody>
+        {page.map((row: any, i: number) => {
+          prepareRow(row);
+          return (
+            <TableRow
+              key={`table-row-${i}`}
+              {...row.getRowProps()}
+              className={"truncate cursor-pointer"}
+              hover={!invoiceTable}
+              onClick={(ev: any) => onRowClick(ev, row)}
+            >
+              {row.cells.map((cell: any, cindex: number) => {
+                return (
+                  <TableCell
+                    key={`table-cell-${cindex}`}
+                    {...cell.getCellProps()}
+                    className={clsx(
+                      "p-12",
+                      cell.column.className,
+                      `${
+                        cell.column.borderRight
+                          ? "cell-border-right cursor-default"
+                          : ""
+                      }`
+                    )}
+                    style={{
+                      width: cell.column.width || "auto",
+                    }}
+                  >
+                    {cell.render("Cell")}
+                  </TableCell>
+                );
+              })}
+            </TableRow>
+          );
+        })}
+      </TableBody>
+      {pagination ? (
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              ActionsComponent={BCTablePagination}
+              SelectProps={{
+                inputProps: { "aria-label": "rows per page" },
+                native: false,
+              }}
+              classes={{
+                root: "overflow-hidden",
+                spacer: "w-0 max-w-0",
+              }}
+              colSpan={5}
+              count={data.length}
+              onChangePage={handleChangePage}
+              onChangeRowsPerPage={handleChangeRowsPerPage}
+              page={isPageSaveEnabled ? currentPage : pageIndex}
+              rowsPerPage={pageSize}
+              rowsPerPageOptions={[
+                5,
+                10,
+                25,
+                {
+                  label: "All",
+                  value: data.length + 1,
+                },
+              ]}
+            />
+          </TableRow>
+        </TableFooter>
+      ) : null}
+    </MaUTable>
+  );
 
   // Render the UI for your table
   return (
@@ -84,106 +189,13 @@ function BCTableContent({
       }`}
       component={Paper}
     >
-      <MaUTable size={"small"} {...getTableProps()}>
-        <TableHead>
-          {headerGroups.map((headerGroup: any, gindex: number) => (
-            <TableRow
-              key={`table-${gindex}`}
-              {...headerGroup.getHeaderGroupProps()}
-            >
-              {headerGroup.headers.map((column: any, hindex: number) => (
-                <TableCell
-                  className={`whitespace-no-wrap p-12 ${
-                    column.borderRight ? "cell-border-right cursor-default" : ""
-                  }`}
-                  key={`table-cell-${hindex}`}
-                  {...(!column.sortable
-                    ? column.getHeaderProps()
-                    : column.getHeaderProps(column.getSortByToggleProps()))}
-                >
-                  {column.render("Header")}
-                  {column.sortable ? (
-                    <TableSortLabel
-                      active={column.isSorted}
-                      // React-table has a unsorted state which is not treated here
-                      direction={column.isSortedDesc ? "desc" : "asc"}
-                    />
-                  ) : null}
-                </TableCell>
-              ))}
-            </TableRow>
-          ))}
-        </TableHead>
-        <TableBody>
-          {page.map((row: any, i: number) => {
-            prepareRow(row);
-            return (
-              <TableRow
-                key={`table-row-${i}`}
-                {...row.getRowProps()}
-                className={"truncate cursor-pointer"}
-                hover={!invoiceTable}
-                onClick={(ev: any) => onRowClick(ev, row)}
-              >
-                {row.cells.map((cell: any, cindex: number) => {
-                  return (
-                    <TableCell
-                      key={`table-cell-${cindex}`}
-                      {...cell.getCellProps()}
-                      className={clsx(
-                        "p-12",
-                        cell.column.className,
-                        `${
-                          cell.column.borderRight
-                            ? "cell-border-right cursor-default"
-                            : ""
-                        }`
-                      )}
-                      style={{
-                        width: cell.column.width || "auto",
-                      }}
-                    >
-                      {cell.render("Cell")}
-                    </TableCell>
-                  );
-                })}
-              </TableRow>
-            );
-          })}
-        </TableBody>
-        {pagination ? (
-          <TableFooter>
-            <TableRow>
-              <TablePagination
-                ActionsComponent={BCTablePagination}
-                SelectProps={{
-                  inputProps: { "aria-label": "rows per page" },
-                  native: false,
-                }}
-                classes={{
-                  root: "overflow-hidden",
-                  spacer: "w-0 max-w-0",
-                }}
-                colSpan={5}
-                count={data.length}
-                onChangePage={handleChangePage}
-                onChangeRowsPerPage={handleChangeRowsPerPage}
-                page={isPageSaveEnabled ? currentPage : pageIndex}
-                rowsPerPage={pageSize}
-                rowsPerPageOptions={[
-                  5,
-                  10,
-                  25,
-                  {
-                    label: "All",
-                    value: data.length + 1,
-                  },
-                ]}
-              />
-            </TableRow>
-          </TableFooter>
-        ) : null}
-      </MaUTable>
+      {(!!isPageSaveEnabled && showTable) &&
+        <TableContent />
+      }
+
+    {!isPageSaveEnabled &&
+      <TableContent />
+    }
     </TableContainer>
   );
 }
