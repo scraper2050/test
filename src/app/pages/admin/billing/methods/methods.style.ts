e4d70e9e7112bc306e86cefcdb9dc2b09727b3ff
Arslan Sajid <arslansajid951@gmail.com>
2021-01-28 18:19:52 +0500

    Added billing pages

--- src/app/pages/admin/billing/methods/methods.style.ts
+++ src/app/pages/admin/billing/methods/methods.style.ts
@@ -1,2 +1,18 @@
 import { Theme } from '@material-ui/core/styles';
-export default (theme: Theme): any => ({});
+export default (theme: Theme): any => ({
+    'addButtonArea': {
+        'display': 'flex',
+        'justifyContent': 'flex-end',
+        'padding': '12px 30px 5px 0',
+        'position': 'absolute',
+        'right': '0',
+        'zIndex': '1000'
+      },
+    'fabRoot': {
+        'color': '#fff',
+        'fontSize': '.775rem',
+        'height': '34px',
+        'padding': '0 12px',
+        'textTransform': 'capitalize'
+    }
+});
