00f0564e42dde769852d6f57efd85c832c52d048
mac <awonusiolajide@yahoo.com>
2021-03-23 17:11:02 +0100

    scheduled time should be in am/pm

--- src/app/components/bc-date-time-picker/bc-date-time-picker.tsx
+++ src/app/components/bc-date-time-picker/bc-date-time-picker.tsx
@@ -20,7 +20,7 @@ const useStyles = makeStyles(theme => ({
 function BCDateTimePicker({
   handleChange,
   value,
-  ampm = false,
+  ampm = true,
   views = ['hours', 'minutes', 'seconds'],
   disabled = false,
   disablePast = false,
