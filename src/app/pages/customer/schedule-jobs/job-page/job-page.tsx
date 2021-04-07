1859054861eed8644445447f626f1ccd11defe5b
Kurt Lopez <kurtdevph@gmail.com>
2021-04-08 03:27:36 +0800

    Add catch for contractor and technician key for vendors

--- src/app/pages/customer/schedule-jobs/job-page/job-page.tsx
+++ src/app/pages/customer/schedule-jobs/job-page/job-page.tsx
@@ -1,20 +1,22 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import Fab from "@material-ui/core/Fab";
-import { withStyles, Grid } from "@material-ui/core";
-import styled from "styled-components";
-
-import BCTableContainer from "../../../../components/bc-table-container/bc-table-container";
-import { getAllJobsAPI } from "api/job.api";
-import { modalTypes } from "../../../../../constants";
-import styles from "../../customer.styles";
-import { formatDate, convertMilitaryTime } from "helpers/format";
-import {
-  openModalAction,
-  setModalDataAction,
-  closeModalAction
-} from "actions/bc-modal/bc-modal.action";
+import BCTableContainer from '../../../../components/bc-table-container/bc-table-container';
+import Fab from '@material-ui/core/Fab';
 import InfoIcon from '@material-ui/icons/Info';
+import { getAllJobsAPI } from 'api/job.api';
+import { modalTypes } from '../../../../../constants';
+import styled from 'styled-components';
+import styles from '../../customer.styles';
+import { Grid, withStyles } from '@material-ui/core';
+import React, { useEffect } from 'react';
+import { convertMilitaryTime, formatDate } from 'helpers/format';
+import {
+  // CloseModalAction,
+  openModalAction,
+  setModalDataAction
+} from 'actions/bc-modal/bc-modal.action';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+// Import { VendorsReducer } from 'reducers/vendor.reducer';
 
 
 interface StatusTypes {
@@ -23,195 +25,220 @@ interface StatusTypes {
 
 function JobPage({ classes, currentPage, setCurrentPage }: any) {
   const dispatch = useDispatch();
-  const { isLoading = true, jobs, refresh = true } = useSelector(
-    ({ jobState }: any) => ({
-      isLoading: jobState.isLoading,
-      jobs: jobState.data,
-      refresh: jobState.refresh,
-    })
-  );
+  const { isLoading = true, jobs, refresh = true } = useSelector(({ jobState }: any) => ({
+    'isLoading': jobState.isLoading,
+    'jobs': jobState.data,
+    'refresh': jobState.refresh
+  }));
 
-  const RenderStatus = ({ status }: StatusTypes) => {
-    const textStatus = status === 0 ? 'Scheduled' : status === 1 ? 'Started' : status === 2 ? 'Finished' : 'Canceled';
-    return <div className={
-      status === 0 ? classes.statusPendingText :
-        status === 1 ? classes.statusStartedText :
-          status === 2 ? classes.statusFinishedText :
-            classes.statusCanceledText}>
+  function RenderStatus({ status }: StatusTypes) {
+    const statusArray = [
+      {
+        'class': classes.statusPendingText,
+        'text': 'Scheduled'
+      },
+      {
+        'class': classes.statusStartedText,
+        'text': 'Started'
+      },
+      {
+        'class': classes.statusFinishedText,
+        'text': 'Finished'
+      },
+      {
+        'class': classes.statusCanceledText,
+        'text': 'Canceled'
+      }
+    ];
+    const textStatus = statusArray[status].text;
+    return <div className={statusArray[status].class}>
       {textStatus}
-    </div>
+    </div>;
+  }
+
+  function RenderVendor({ vendor }:any) {
+    if (vendor) {
+      return vendor.profile
+        ? vendor.profile.displayName
+        : vendor.info.companyName;
+    }
+    return null;
   }
 
   const openEditJobModal = (job: any) => {
-    dispatch(
-      setModalDataAction({
-        data: {
-          job: job,
-          modalTitle: "Edit Job",
-          removeFooter: false,
-        },
-        type: modalTypes.EDIT_JOB_MODAL,
-      })
-    );
+    dispatch(setModalDataAction({
+      'data': {
+        'job': job,
+        'modalTitle': 'Edit Job',
+        'removeFooter': false
+      },
+      'type': modalTypes.EDIT_JOB_MODAL
+    }));
     setTimeout(() => {
       dispatch(openModalAction());
     }, 200);
   };
 
 
-  const closeModal = () => {
-    dispatch(closeModalAction());
-    setTimeout(() => {
-      dispatch(setModalDataAction({
-        'data': {},
-        'type': ''
-      }));
-    }, 200);
-  };
+  /*
+   * Const closeModal = () => {
+   *   dispatch(closeModalAction());
+   *   setTimeout(() => {
+   *     dispatch(setModalDataAction({
+   *       'data': {},
+   *       'type': ''
+   *     }));
+   *   }, 200);
+   * };
+   */
 
   const openDetailJobModal = (job: any) => {
-    dispatch(
-      setModalDataAction({
-        data: {
-          job: job,
-          detail: true,
-          modalTitle: "View Job",
-          removeFooter: false,
-        },
-        type: modalTypes.EDIT_JOB_MODAL,
-      })
-    );
+    dispatch(setModalDataAction({
+      'data': {
+        'detail': true,
+        'job': job,
+        'modalTitle': 'View Job',
+        'removeFooter': false
+      },
+      'type': modalTypes.EDIT_JOB_MODAL
+    }));
     setTimeout(() => {
       dispatch(openModalAction());
     }, 200);
   };
 
   const formatSchedulingTime = (time: string) => {
-    let timeAr = time.split("T");
-    let timeWithSeconds = timeAr[1].substr(0, 5);
-    let hours = timeWithSeconds.substr(0, 2);
-    let minutes = timeWithSeconds.substr(3, 5);
+    const timeAr = time.split('T');
+    const timeWithSeconds = timeAr[1].substr(0, 5);
+    const hours = timeWithSeconds.substr(0, 2);
+    const minutes = timeWithSeconds.substr(3, 5);
 
-    return { hours, minutes };
+    return { hours,
+      minutes };
   };
 
   const columns: any = [
     {
-      Header: "Job ID",
-      accessor: "jobId",
-      className: "font-bold",
-      sortable: true,
-      width: 50
+      'Header': 'Job ID',
+      'accessor': 'jobId',
+      'className': 'font-bold',
+      'sortable': true,
+      'width': 50
     },
     {
       Cell({ row }: any) {
-        return <RenderStatus status={row.original.status} />
+        return <RenderStatus status={row.original.status} />;
       },
-      Header: "Status",
-      accessor: "status",
-      className: "font-bold",
-      sortable: true,
-      width: 60
+      'Header': 'Status',
+      'accessor': 'status',
+      'className': 'font-bold',
+      'sortable': true,
+      'width': 60
     },
     {
-      Header: "Technician",
-      accessor: "technician.profile.displayName",
-      className: "font-bold",
-      sortable: true,
+      Cell({ row }: any) {
+        return <RenderVendor vendor={row.original.technician || row.original.contractor} />;
+      },
+      'Header': 'Technician',
+      'accessor': 'contractor.info.companyName',
+      'className': 'font-bold',
+      'sortable': true
     },
     {
-      Header: "Customer",
-      accessor: "customer.profile.displayName",
-      className: "font-bold",
-      sortable: true,
+      'Header': 'Customer',
+      'accessor': 'customer.profile.displayName',
+      'className': 'font-bold',
+      'sortable': true
     },
     {
-      Header: "Type",
-      accessor: "type.title",
-      className: "font-bold",
-      sortable: true,
+      'Header': 'Type',
+      'accessor': 'type.title',
+      'className': 'font-bold',
+      'sortable': true
     },
     {
       Cell({ row }: any) {
         const scheduleDate = formatDate(row.original.scheduleDate);
         return (
-          <div className={"flex items-center"}>
-            <p>{scheduleDate}</p>
+          <div className={'flex items-center'}>
+            <p>
+              {scheduleDate}
+            </p>
           </div>
         );
       },
-      Header: "Schedule Date",
-      id: "job-schedulee-date",
-      sortable: true,
-      width: 60
+      'Header': 'Schedule Date',
+      'id': 'job-schedulee-date',
+      'sortable': true,
+      'width': 60
     },
     {
       Cell({ row }: any) {
-        let startTime = "N/A";
-        let endTime = "N/A";
+        let startTime = 'N/A';
+        let endTime = 'N/A';
         if (row.original.scheduledStartTime !== undefined) {
-          let formatScheduledObj = formatSchedulingTime(
-            row.original.scheduledStartTime
-          );
-          startTime = convertMilitaryTime(
-            `${formatScheduledObj.hours}:${formatScheduledObj.minutes}`
-          );
+          const formatScheduledObj = formatSchedulingTime(row.original.scheduledStartTime);
+          startTime = convertMilitaryTime(`${formatScheduledObj.hours}:${formatScheduledObj.minutes}`);
         }
         if (row.original.scheduledEndTime !== undefined) {
-          let formatScheduledObj = formatSchedulingTime(
-            row.original.scheduledEndTime
-          );
-          endTime = convertMilitaryTime(
-            `${formatScheduledObj.hours}:${formatScheduledObj.minutes}`
-          );
+          const formatScheduledObj = formatSchedulingTime(row.original.scheduledEndTime);
+          endTime = convertMilitaryTime(`${formatScheduledObj.hours}:${formatScheduledObj.minutes}`);
         }
         return (
-          <div className={"flex items-center"} >
-            <p>{`${startTime} - ${endTime}`}</p>
+          <div className={'flex items-center'} >
+            <p>
+              {`${startTime} - ${endTime}`}
+            </p>
           </div>
         );
       },
-      Header: "Time",
-      id: "job-time",
-      sortable: true,
-      width: 40
+      'Header': 'Time',
+      'id': 'job-time',
+      'sortable': true,
+      'width': 40
     },
     {
       Cell({ row }: any) {
         return (
-          <Grid container alignItems="center">
-            <div className={"flex items-center"}>
-              {row.original.status === 0 && !row.original.employeeType ? (
-                <Fab
-                  aria-label={"edit-job"}
+          <Grid
+            alignItems={'center'}
+            container>
+            <div className={'flex items-center'}>
+              {row.original.status === 0 && !row.original.employeeType
+                ? <Fab
+                  aria-label={'edit-job'}
                   classes={{
-                    root: classes.fabRoot,
+                    'root': classes.fabRoot
                   }}
-                  color={"primary"}
+                  color={'primary'}
                   onClick={() => openEditJobModal(row.original)}
-                  variant={"extended"}
-                >
-                  {"Edit"}
+                  variant={'extended'}>
+                  {'Edit'}
                 </Fab>
-              ) : <div style={{ width: 48, height: 34 }} />}
+                : <div style={{
+                  'height': 34,
+                  'width': 48 }}
+                />}
             </div>
             <div
+              className={'flex items-center'}
               onClick={() => openDetailJobModal(row.original)}
               style={{
-                marginLeft: '.5rem', height: 34,
-                display: 'flex',
-                alignItems: 'center',
-              }}
-              className={'flex items-center'}>
-              <InfoIcon style={{ margin: 'auto, 0' }} />
+                'alignItems': 'center',
+                'display': 'flex',
+                'height': 34,
+                'marginLeft': '.5rem'
+
+              }}>
+              <InfoIcon style={{ 'margin': 'auto, 0' }} />
             </div>
           </Grid>
         );
       },
-      Header: "Options",
-      id: "action-options",
-      sortable: false,
-    },
+      'Header': 'Options',
+      'id': 'action-options',
+      'sortable': false
+    }
   ];
 
   useEffect(() => {
@@ -223,13 +250,13 @@ function JobPage({ classes, currentPage, setCurrentPage }: any) {
   const handleRowClick = (event: any, row: any) => { };
 
   return (
-    <DataContainer id={"0"}>
+    <DataContainer id={'0'}>
       <BCTableContainer
         columns={columns}
         isLoading={isLoading}
         onRowClick={handleRowClick}
         search
-        searchPlaceholder={"Search Jobs..."}
+        searchPlaceholder={'Search Jobs...'}
         tableData={jobs}
       />
     </DataContainer>
@@ -243,4 +270,4 @@ const DataContainer = styled.div`
   overflow: hidden;
 `;
 
-export default withStyles(styles, { withTheme: true })(JobPage);
+export default withStyles(styles, { 'withTheme': true })(JobPage);
