72b00c4a7e3fba1371ef069fd1d51604844970a0
Kurt Lopez <kurtdevph@gmail.com>
2021-06-12 00:03:13 +0800

    add reset customer to prevent invoice price bug

--- src/actions/customer/customer.action.ts
+++ src/actions/customer/customer.action.ts
@@ -1,12 +1,12 @@
-import { createApiAction } from "../action.utils";
-import { CustomersActionType, types } from "../../reducers/customer.types";
+import { createApiAction } from '../action.utils';
+import { CustomersActionType, types } from '../../reducers/customer.types';
 import {
-  getCustomers as fetchCustomers,
-  updateCustomers,
-  getCustomerDetail,
   createCustomer,
-} from "api/customer.api";
-import { info, error } from "actions/snackbar/snackbar.action";
+  getCustomers as fetchCustomers,
+  getCustomerDetail,
+  updateCustomers
+} from 'api/customer.api';
+import { error, info } from 'actions/snackbar/snackbar.action';
 
 export const loadCustomersActions = createApiAction(types.CUSTOMER_LOAD);
 export const newCustomerAction = createApiAction(types.CUSTOMER_NEW);
@@ -14,12 +14,12 @@ export const deleteCustomerActions = createApiAction(types.CUSTOMER_REMOVE);
 
 export const loadingCustomers = () => {
   return {
-    type: CustomersActionType.GET,
+    'type': CustomersActionType.GET
   };
 };
 export const loadingSingleCustomers = () => {
   return {
-    type: types.GET_SINGLE_CUSTOMER,
+    'type': types.GET_SINGLE_CUSTOMER
   };
 };
 
@@ -37,8 +37,8 @@ export const getCustomers = () => {
 
 export const setCustomers = (customers: any) => {
   return {
-    type: types.SET_CUSTOMERS,
-    payload: customers,
+    'type': types.SET_CUSTOMERS,
+    'payload': customers
   };
 };
 
@@ -46,23 +46,57 @@ export const getCustomerDetailAction = (data: any) => {
   return async (dispatch: any) => {
     const customer: any = await getCustomerDetail(data);
 
-    dispatch({ type: types.SET_SINGLE_CUSTOMER, payload: customer });
+    dispatch({ 'type': types.SET_SINGLE_CUSTOMER,
+      'payload': customer });
   };
 };
+
+export const resetCustomer = () => {
+  return async (dispatch: any) => {
+    dispatch({ 'type': types.SET_SINGLE_CUSTOMER,
+      'payload': {
+        '_id': '',
+        'info': {
+          'name': '',
+          'email': ''
+        },
+        'address': {
+          'street': '',
+          'city': '',
+          'state': '',
+          'zipCode': ''
+        },
+        'contact': {
+          'name': '',
+          'phone': ''
+        },
+        'isActive': false,
+        'company': '',
+        'contactname': '',
+        'profile': {
+          'firstName': '',
+          'lastName': '',
+          'displayName': ''
+        },
+        'vendorId': ''
+      } });
+  };
+};
+
 
 export const updateCustomerAction = (customers: any, callback?: any) => {
   return async (dispatch: any) => {
     const customer: any = await updateCustomers(customers);
-    if (customer.hasOwnProperty("msg")) {
+    if (customer.hasOwnProperty('msg')) {
       dispatch({
-        type: CustomersActionType.UPDATE_CUSTOMER_FAILED,
-        payload: customer.msg,
+        'type': CustomersActionType.UPDATE_CUSTOMER_FAILED,
+        'payload': customer.msg
       });
       callback();
     } else {
       dispatch({
-        type: CustomersActionType.UPDATE_CUSTOMER,
-        payload: customer,
+        'type': CustomersActionType.UPDATE_CUSTOMER,
+        'payload': customer
       });
       callback();
     }
