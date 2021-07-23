910b01493f6b94b3485067317de36e9dbaa82480
Hasan H <hasan-h@ligadata.com>
2021-07-23 16:07:02 +0300

    Handle qb id when undefined

--- src/app/components/bc-qb-sync-status/bc-qb-sync-status.tsx
+++ src/app/components/bc-qb-sync-status/bc-qb-sync-status.tsx
@@ -9,9 +9,14 @@ interface Props {
   classes: any;
 }
 
+
+function isNotEmpty(str: string | undefined | null) {
+  return !(!str || 0 === str.trim().length);
+}
+
 function BCQbSyncStatus({ data, classes }: Props) {
   return (
-    data.quickbookId !== null ?
+    isNotEmpty(data.quickbookId) ?
       <img className={classes.logo}
            alt={'logo'}
            title={'synced with QuickBooks'}
