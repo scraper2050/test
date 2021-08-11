2a1b618100414c443da3f7cdfc7873e045388419
Tareq Aziz <tareqaziz0065@gmail.com>
2021-08-11 23:19:35 +0600

    Updating the tags and table buttons
    Updating the tags and table buttons

--- src/app/pages/invoicing/invoices-list/invoices-list-listing/invoices-list-listing.tsx
+++ src/app/pages/invoicing/invoices-list/invoices-list-listing/invoices-list-listing.tsx
@@ -1,7 +1,7 @@
 import BCTableContainer from '../../../../components/bc-table-container/bc-table-container';
 import styled from 'styled-components';
 import styles from './../invoices-list.styles';
-import { Box, Chip, Typography, withStyles } from '@material-ui/core';
+import { Box, Button, Chip, createStyles, makeStyles, Typography, withStyles } from "@material-ui/core";
 import Fab from '@material-ui/core/Fab';
 import React, { useEffect } from 'react';
 import {
@@ -12,19 +12,66 @@ import { useDispatch, useSelector } from 'react-redux';
 import TableFilterService from 'utils/table-filter';
 import { openModalAction, setModalDataAction } from 'actions/bc-modal/bc-modal.action';
 import { modalTypes } from '../../../../../constants';
-import { FormDefaultValues } from 'app/components/bc-shared-form/bc-shared-form-default-values';
-import { FormTypes } from 'app/components/bc-shared-form/bc-shared-form.types';
+import { MailOutlineOutlined } from '@material-ui/icons';
+import VisibilityIcon from '@material-ui/icons/Visibility';
 import EmailInvoiceButton from '../email.invoice';
 import { formatDatTimelll } from 'helpers/format';
 import BCQbSyncStatus from "../../../../components/bc-qb-sync-status/bc-qb-sync-status";
+import { Theme } from "@material-ui/core/styles";
+import * as CONSTANTS from "../../../../../constants";
 
 const getFilteredList = (state: any) => {
   return TableFilterService.filterByDateDesc(state?.invoiceList?.data);
 };
 
+const useCustomStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    // items table
+    centerContainer: {
+      flex: 1,
+      display: 'flex',
+      justifyContent: 'space-around',
+      alignItems: 'center'
+    },
+    iconBtn: {
+      fontSize: 14,
+      color: CONSTANTS.PRIMARY_WHITE
+    },
+    csChip: {
+      borderRadius: 2
+    }
+  }),
+);
+
+
+const CSButton = withStyles({
+  root: {
+    textTransform: 'none',
+    fontSize: 13,
+    padding: '5px 5px',
+    lineHeight: 1.5,
+    minWidth: 40,
+    color: CONSTANTS.PRIMARY_WHITE,
+    backgroundColor: CONSTANTS.TABLE_ACTION_BUTTON,
+    borderColor: CONSTANTS.TABLE_ACTION_BUTTON,
+    '&:hover': {
+      backgroundColor: CONSTANTS.TABLE_ACTION_BUTTON_HOVER,
+      borderColor: CONSTANTS.TABLE_ACTION_BUTTON_HOVER,
+    },
+    '&:active': {
+      backgroundColor: CONSTANTS.TABLE_ACTION_BUTTON_HOVER,
+      borderColor: CONSTANTS.TABLE_ACTION_BUTTON_HOVER,
+    },
+    '&:focus': {
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
+    },
+  },
+})(Button);
+
 function InvoicingListListing({ classes, theme }: any) {
   const dispatch = useDispatch();
   const invoiceList = useSelector(getFilteredList);
+  const customStyles = useCustomStyles()
   const isLoading = useSelector((state: any) => state?.invoiceList?.loading);
   const showInvoiceDetail = (id:string) => {
     dispatch(setModalDataAction({
@@ -105,18 +152,23 @@ function InvoicingListListing({ classes, theme }: any) {
       return row.original.paid
         ? <Chip
           label={'Paid'}
+          size="small"
+          className={customStyles.csChip}
           style={{ 'backgroundColor': theme.palette.success.light,
             'color': '#fff' }}
         />
         : <Chip
+          className={customStyles.csChip}
           color={'secondary'}
+          size="small"
           label={'Unpaid'}
         />;
     },
     'Header': 'Payment Status',
     'accessor': 'paid',
     'className': 'font-bold',
-    'sortable': true
+    'sortable': true,
+      'width': 20
     },
     { Cell({ row }: any) {
       return row.original.lastEmailSent
@@ -141,44 +193,33 @@ function InvoicingListListing({ classes, theme }: any) {
     },
     {
       'Cell'({ row }: any) {
-        return <div className={'flex items-center'}>
-          <Box
-            display={'block'}
-            pr={1}>
-            <EmailInvoiceButton
-              Component={<Fab
-                aria-label={'send-email'}
-                classes={{
-                  'root': classes.fabRoot
-                }}
-                style={{width: '100px'}}
-                color={'primary'}
-                size={'medium'}
-                variant={'extended'}>
-                {'Email'}
-              </Fab>}
-              invoice={row.original}
+        return <div className={customStyles.centerContainer}>
+          <EmailInvoiceButton
+            Component={<CSButton
+              variant="contained"
+              color="primary"
+              size="small">
+              <MailOutlineOutlined
+                className={customStyles.iconBtn}
+              />
+            </CSButton>}
+            invoice={row.original}
+          />
+          <CSButton
+            aria-label={'view more'}
+            variant="contained"
+            color="primary"
+            onClick={() => showInvoiceDetail(row.original._id)}>
+            <VisibilityIcon
+              className={customStyles.iconBtn}
             />
-          </Box>
-          <Fab
-            aria-label={'view-more'}
-            classes={{
-              'root': classes.fabRoot
-            }}
-            color={'primary'}
-            onClick={() => showInvoiceDetail(row.original._id)}
-            size={'medium'}
-            style={{marginTop: '5px', width: '100px'}}
-            variant={'extended'}>
-            {'View More'}
-
-          </Fab>
+          </CSButton>
         </div>;
       },
       'Header': 'Actions',
       'id': 'action-send-email',
       'sortable': false,
-      'width': 60
+      'width': 120
     },
     {
       Cell({ row }: any) {
