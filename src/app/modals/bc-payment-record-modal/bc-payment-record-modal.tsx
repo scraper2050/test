1150dbda7fb6c5fb54f662f9c4c25ef36e6ac60b
Hasan H <hasan-h@ligadata.com>
2021-08-26 13:14:20 +0300

    Hide number arrows in record payment comp. only

--- src/app/modals/bc-payment-record-modal/bc-payment-record-modal.tsx
+++ src/app/modals/bc-payment-record-modal/bc-payment-record-modal.tsx
@@ -381,6 +381,18 @@ const DataContainer = styled.div`
   .save-customer-button {
     color: ${CONSTANTS.PRIMARY_WHITE};
   }
+  
+  /* Chrome, Safari, Edge, Opera */
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+  
+  /* Firefox */
+  input[type=number] {
+    -moz-appearance: textfield;
+  }
 `;
 
 export default withStyles(
