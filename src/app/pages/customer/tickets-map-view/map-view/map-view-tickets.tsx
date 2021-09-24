bffa4724b2f23d7e1ac44ea6bd0fe34eb73e0f03
Jorje Barrera <w.jbarreraact@gmail.com>
2021-09-24 22:30:09 +0800

    Fix pin going missing when navigating to other pages.

--- src/app/pages/customer/tickets-map-view/map-view/map-view-tickets.tsx
+++ src/app/pages/customer/tickets-map-view/map-view/map-view-tickets.tsx
@@ -38,6 +38,8 @@ import {
 import { getAllJobTypesAPI } from "api/job.api";
 import { modalTypes } from "../../../../../constants";
 import { getCustomers } from "actions/customer/customer.action";
+import { isObjectEmpty } from 'utils/objects';
+
 const PAGE_SIZE = 4;
 
 function MapViewTicketsScreen({ classes }: any) {
@@ -362,7 +364,7 @@ function MapViewTicketsScreen({ classes }: any) {
   return (
     <Grid container item lg={12}>
       <Grid container item lg={6} className="ticketsMapContainer">
-        {
+        {!isObjectEmpty(selectedTicket) &&
           <MemoizedMap
             list={openTicketsClone}
             selected={selectedTicket}
