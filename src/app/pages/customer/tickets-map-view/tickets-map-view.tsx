ebf4e2299024294e5359b68bb5220c77deb3baf3
Kam Curacha <kamcuracha@yahoo.com>
2021-10-20 00:29:03 +0800

    feature(ui): #335 wip recreate routes tab with copied sidebar

--- src/app/pages/customer/tickets-map-view/tickets-map-view.tsx
+++ src/app/pages/customer/tickets-map-view/tickets-map-view.tsx
@@ -7,6 +7,7 @@ import './ticket-map-view.scss';
 import MapViewTicketsScreen from './map-view/map-view-tickets';
 import MapViewTodayJobsScreen from './map-view/map-view-today-jobs';
 import MapViewJobsScreen from './map-view/map-view-jobs';
+import MapViewRoutesScreen from './map-view/map-view-routes';
 import { getAllJobsAPI } from 'api/job.api';
 import { useDispatch } from 'react-redux';
 import BCCircularLoader from 'app/components/bc-circular-loader/bc-circular-loader';
@@ -138,10 +139,10 @@ function TicketsWithMapView({ classes }: any) {
                   'label': 'Scheduled Jobs',
                   'value': 2
                 },
-                // {
-                //   'label': 'Routes',
-                //   'value': 3
-                // }
+                {
+                  'label': 'Routes',
+                  'value': 3
+                },
               ]}
             />
             { renderLegend() }
@@ -163,6 +164,12 @@ function TicketsWithMapView({ classes }: any) {
                 hidden={curTab !== 2}
                 id={'map-swipeable-schedule'}>
                 <MapViewJobsScreen />
+              </div>
+              <div
+                className={`${classes.dataContainer} ${classes.dataContainer}_maps`}
+                hidden={curTab !== 3}
+                id={'map-swipeable-routes'}>
+                <MapViewRoutesScreen />
               </div>
             </SwipeableViews>
           </div>
