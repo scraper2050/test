28debe4f2cd30347c7ed9f85da2b350c863079f4
Hasan H <hasan-h@ligadata.com>
2022-03-04 01:43:23 +0200

    Clean styles

--- src/app/pages/admin/vendors/view-more/view-more.styles.tsx
+++ src/app/pages/admin/vendors/view-more/view-more.styles.tsx
@@ -8,31 +8,4 @@ export default (theme: Theme): any => ({
   ...topActionBar,
   ...dataContainer,
   ...swipe_wrapper,
-
-  rangePickerContainer: {
-    position: 'relative',
-  },
-  rangePickerButton: {
-    textTransform: 'none',
-    borderRadius: 8,
-  },
-  rangePickerPopup: {
-    zIndex: 1,
-    padding: 4,
-    borderRadius: 8,
-    border: '1px solid #E0E0E0',
-    boxShadow: '3px 3px 3px #E0E0E088',
-    backgroundColor: 'white',
-  },
-  rangePickerWrapper: {
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  rangePicker: {
-    borderBottom: '1px solid #E0E0E0',
-  },
-  buttonsWrapper: {
-    alignSelf: 'flex-end',
-    padding: 16,
-  }
 });
