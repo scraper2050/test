98918bafe749325b1a7e8d07040012174e750262
Aldy Andika <aldy.andika@gmail.com>
2022-03-10 02:02:34 +0700

    filter contacts based on job location on Service Ticket Modal

--- src/app/modals/bc-service-ticket-modal/bc-service-ticket-modal.tsx
+++ src/app/modals/bc-service-ticket-modal/bc-service-ticket-modal.tsx
@@ -115,7 +115,9 @@ function BCServiceTicketModal({
 
     await setFieldValue(fieldName, '');
     await setFieldValue('jobSiteId', '');
+    await setFieldValue('customerContactId', '');
     await setJobSiteValue([]);
+    await setContactValue([]);
     await setJobLocationValue(newValue);
     if (locationId !== '') {
       await dispatch(getJobSites({ customerId,
@@ -611,7 +613,21 @@ function BCServiceTicketModal({
                   getOptionLabel={option => option.name ? option.name : ''}
                   id={'tags-standard'}
                   onChange={(ev: any, newValue: any) => handleContactChange(ev, 'customerContactId', setFieldValue, newValue)}
-                  options={contacts && contacts.length !== 0 ? contacts.sort((a: any, b: any) => a.name > b.name ? 1 : b.name > a.name ? -1 : 0) : []}
+                  options={
+                    contacts && contacts.length !== 0
+                      ? FormikValues.jobLocationId && jobLocationValue && jobLocationValue.contacts 
+                        ? jobLocationValue.contacts.filter((contact:any) => 
+                            contact.isActive
+                          ).sort((a: any, b: any) =>
+                            a.name > b.name ? 1 : b.name > a.name ? -1 : 0
+                          )
+                        : contacts.filter((contact:any) => 
+                            contact.isActive
+                          ).sort((a: any, b: any) =>
+                            a.name > b.name ? 1 : b.name > a.name ? -1 : 0
+                          )
+                      : []
+                  }
                   renderInput={params =>
                     <TextField
                       {...params}
