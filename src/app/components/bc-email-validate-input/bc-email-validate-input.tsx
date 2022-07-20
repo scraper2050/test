22e0b21ee186be722a70c98f0afc715e106a4708
Aldy Andika <aldy.andika@gmail.com>
2022-07-21 03:58:44 +0700

    add recovery email field

--- src/app/components/bc-email-validate-input/bc-email-validate-input.tsx
+++ src/app/components/bc-email-validate-input/bc-email-validate-input.tsx
@@ -1,6 +1,14 @@
 import { FormDataModel } from '../../models/form-data';
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import IconButton from '@material-ui/core/IconButton';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import CloseIcon from '@material-ui/icons/Close';
+import Popper from '@material-ui/core/Popper';
+import Fade from '@material-ui/core/Fade';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import validator from 'validator';
 
 
@@ -12,7 +20,58 @@ interface BCEmailValidateInputProps {
   inputData: FormDataModel;
   onChange: Function;
   disabled?: boolean;
+  referenceEmail? : string;
+  infoText? : string;
 }
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    infoContainer: {
+      width: 311,
+      padding: 14,
+      paddingTop: 20,
+      paddingRight: 25,
+      fontSize: 13,
+      color: '#4F4F4F',
+    },
+    closeIcon: {
+      cursor: 'pointer',
+      position: 'absolute',
+      right: 5,
+      top: 5,
+      fontSize: 20,
+    },
+    popper: {
+      zIndex: 1,
+      boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.3)',
+      '&[x-placement*="top"] $arrow': {
+        bottom: 0,
+        left: 0,
+        marginBottom: '-0.9em',
+        width: '3em',
+        height: '1em',
+        '&::before': {
+          borderWidth: '1em 1em 0 1em',
+          borderColor: `${theme.palette.background.paper} transparent transparent transparent`,
+        },
+      },
+    },
+    arrow: {
+      position: 'absolute',
+      fontSize: 10,
+      width: '3em',
+      height: '3em',
+      '&::before': {
+        content: '""',
+        margin: 'auto',
+        display: 'block',
+        width: 0,
+        height: 0,
+        borderStyle: 'solid',
+      },
+    },
+  }),
+);
 
 function BCEmailValidateInput({
   id = 'email',
@@ -21,8 +80,14 @@ function BCEmailValidateInput({
   size = 'small',
   inputData,
   disabled = false,
-  onChange
+  onChange,
+  referenceEmail = '',
+  infoText = '',
 }: BCEmailValidateInputProps) {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [arrowRef, setArrowRef] = React.useState<null | HTMLElement>(null);
+  
   const handleChangeEmail = (e: any) => {
     const emailData = {
       'errorMsg': '',
@@ -32,7 +97,10 @@ function BCEmailValidateInput({
 
     if (e.target.value.length === 0) {
       emailData.validate = false;
-      emailData.errorMsg = 'This field required';
+      emailData.errorMsg = 'This field is required';
+    } else if (emailData.value === referenceEmail) {
+      emailData.validate = false;
+      emailData.errorMsg = 'This email cannot be the same as primary email';
     } else if (validator.isEmail(emailData.value)) {
       emailData.validate = true;
       emailData.errorMsg = '';
@@ -45,6 +113,17 @@ function BCEmailValidateInput({
       ...emailData
     });
   };
+
+  const handleClickShowInfo = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(anchorEl ? null : event.currentTarget);
+  };
+
+  const handleMouseDownInfo = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+
+  const open = Boolean(anchorEl);
+  const popperId = open ? 'transitions-popper' : undefined;
 
   return (
     <TextField
@@ -59,6 +138,52 @@ function BCEmailValidateInput({
       type={'email'}
       value={inputData.value}
       variant={variant}
+      InputProps={{
+        endAdornment: infoText ? (
+          <InputAdornment position="end">
+            <IconButton
+              aria-label="recovery email info"
+              onClick={handleClickShowInfo}
+              onMouseDown={handleMouseDownInfo}
+            >
+              <InfoOutlinedIcon />
+              <Popper
+                id={popperId}
+                open={open}
+                anchorEl={anchorEl}
+                transition
+                placement="top-end"
+                className={classes.popper}
+                modifiers={{
+                  flip: {
+                    enabled: false,
+                  },
+                  preventOverflow: {
+                    enabled: false,
+                    boundariesElement: 'scrollParent',
+                  },
+                  arrow: {
+                    enabled: true,
+                    element: arrowRef,
+                  },
+                }}
+              >
+                {({ TransitionProps }) => (
+                  <Fade {...TransitionProps} timeout={350}>
+                    <>
+                      <span className={classes.arrow} ref={setArrowRef} /> 
+                      <Paper className={classes.infoContainer}>
+                        <CloseIcon className={classes.closeIcon}/>
+                        {infoText}
+                      </Paper>
+                    </>
+                  </Fade>
+                )}
+              </Popper>
+            </IconButton>
+          </InputAdornment>
+        ) : null,
+      }}
     />
   );
 }
