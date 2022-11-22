54b85bdaa5ad0c8266fbe166c27771b577c4fba6
Hasan H <hasan-h@ligadata.com>
2022-11-22 16:39:24 +0300

    Show popups for contact name and address Show totals on a separate line

---
+++ src/app/components/bc-data-grid/bc-popper.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+import styled from "styled-components";
+import {GRAY3, PRIMARY_BLUE} from "../../../constants";
+import {ReferenceObject} from "popper.js";
+import {Popper, Typography} from "@material-ui/core";
+import EmailIcon from "@material-ui/icons/Email";
+import PhoneIcon from "@material-ui/icons/Phone";
+
+interface PROPS{
+  type: string;
+  data: any;
+  anchor: null | ReferenceObject;
+}
+
+export const BcPopper = ({type, data, anchor}: PROPS) => {
+  const open = Boolean(anchor);
+  const id = open ? 'simple-popover' : undefined;
+  console.log(data, anchor);
+
+  return data && <Popper
+    id={id}
+    placement="top"
+    open={open}
+    anchorEl={anchor}
+    modifiers={{
+      flip: {
+        enabled: false,
+      },
+    }}
+  >
+    <Container>
+      {type === 'contact' ? <Contact data={data}/> : <Address data={data} />}
+    </Container>
+  </Popper>
+}
+
+
+const Container = styled.div<{isBold?: boolean}>`
+  background-color: #fff;
+  padding: 15px 25px;
+  height: auto;
+  position: relative;
+  border-radius: 10px;
+  box-shadow: 0 2px 6px rgba(0,0,0,0.15);
+  &::after {
+    content: "";
+    position: absolute;
+    bottom: -12px;
+    left: calc(50% - 12px);
+    border-left: 12px solid transparent;
+    border-right: 12px solid transparent;
+    border-top: 12px solid #c4c4c4;
+  }
+  &::before {
+    content: "";
+    position: absolute;
+    bottom: -11px;
+    left: calc(50% - 10px);
+    border-left: 10px solid transparent;
+    border-right: 10px solid transparent;
+    border-top: 11px solid #ffffff;
+    border-bottom-left-radius: 5px;
+    z-index: 1;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+    p {
+      margin: 0 8px;
+      color: ${GRAY3};
+      font-size: 14px;
+    }
+  }
+
+`;
+
+const Contact = ({data}: any) => <>
+  <Typography variant="h6">{data.name}</Typography>
+  <div>
+    <EmailIcon style={{color: GRAY3, fontSize: 14}}/><p>{data.email}</p>
+  </div>
+  <div>
+    <PhoneIcon style={{color: GRAY3, fontSize: 14}}/><p>{data.phone}</p>
+  </div>
+</>
+
+
+const Address = ({data}: any) => <>
+  <Typography variant="h6">{data.name}</Typography>
+  <span>{data.street}<br/>{data.city}<br/>{data.state}, {data.zipcode}</span>
+</>
