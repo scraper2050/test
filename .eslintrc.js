62767125d1d72df9f81d6c00d88ba2bb68e555ba
Hasan H <dr.mhhashem@gmail.com>
2022-12-19 13:16:26 +0300

    Hide chat unread count when tab opens

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
     // "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     "react-app",
     "eslint:all",
-    "plugin:react/all",
+    // "plugin:react/all",
   ],
   rules: {
     "linebreak-style": 0,
