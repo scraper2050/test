aa7b978ad56dd0d7a9319fc4fbbd366ba4d299d3
Jorge Galvis Cardenas <jorge@allcode.com>
2023-06-09 12:22:12 -0500

    Fixing problem on search service tickets

--- src/app/pages/customer/schedule-jobs/service-ticket/service-ticket.tsx
+++ src/app/pages/customer/schedule-jobs/service-ticket/service-ticket.tsx
@@ -369,20 +369,20 @@ function ServiceTicket({ classes, hidden }: any) {
                 toolbarPositionLeft={true}
                 toolbar={Toolbar()}
                 manualPagination
-                fetchFunction={(num: number, isPrev:boolean, isNext:boolean, query :string) =>{}
-                  //dispatch(getAllServiceTicketsAPI(num || currentPageSize, isPrev ? prevCursor : undefined, isNext ? nextCursor : undefined, showAllTickets, query === '' ? '' : query || keyword, selectionRange))
-                }
                 total={total}
                 currentPageIndex={currentPageIndex}
                 setCurrentPageIndexFunction={(num: number, apiCall: boolean) => {
                   dispatch(setCurrentPageIndex(num))
-                  if(apiCall){
-                    dispatch(getAllServiceTicketsAPI(currentPageSize, num,  showAllTickets,keyword, selectionRange, currentDivision.params))
+                  if (apiCall) {
+                    dispatch(getAllServiceTicketsAPI(currentPageSize, num, showAllTickets, keyword, selectionRange, currentDivision.params))
                   }
                 }}
                 currentPageSize={currentPageSize}
                 setCurrentPageSizeFunction={(num: number) => dispatch(setCurrentPageSize(num))}
-                setKeywordFunction={(query: string) => dispatch(setKeyword(query))}
+                setKeywordFunction={(query: string) => {
+                    dispatch(setKeyword(query));
+                    dispatch(getAllServiceTicketsAPI(currentPageSize, currentPageIndex, showAllTickets, query, selectionRange, currentDivision.params));
+                  }}
               />
             </div>
             <div
