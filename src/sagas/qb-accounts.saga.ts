120bca6fe1ff610259c33eca043eb544fd6cdab8
Usama Zulfiqar <usama.sama@gmail.com>
2023-09-15 18:55:24 +0500

    BL2-280: (fix) build saga removed

--- src/sagas/qb-accounts.saga.ts
+++ src/sagas/qb-accounts.saga.ts
@@ -4,22 +4,23 @@ import { all, call, cancelled, put, takeLatest } from 'redux-saga/effects';
 
 
 export function* handleGetQBAccount(action: { payload: any }) {
-  try {
-    const [qbAccountsResult]:any = yield all([
-      call(quickbooksGetAccounts, action.payload)
-    ]);
-    yield all([
-      put(loadQBAccounts.success(qbAccountsResult.data.accounts))
-    ]);
-  } catch (error) {
-    yield all([
-      put(loadQBAccounts.fault(error.toString()))
-    ]);
-  } finally {
-    if (yield cancelled()) {
-      yield put(loadQBAccounts.cancelled());
-    }
-  }
+  // pending integrations not in use currently
+  // try {
+  //   const [qbAccountsResult]:any = yield all([
+  //     call(quickbooksGetAccounts, action.payload)
+  //   ]);
+  //   yield all([
+  //     put(loadQBAccounts.success(qbAccountsResult.data.accounts))
+  //   ]);
+  // } catch (error) {
+  //   yield all([
+  //     put(loadQBAccounts.fault(error.toString()))
+  //   ]);
+  // } finally {
+  //   if (yield cancelled()) {
+  //     yield put(loadQBAccounts.cancelled());
+  //   }
+  // }
 }
 
 
